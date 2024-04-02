577a1d753f18ebff8bb80d4cdef3eba2c27375ef
RoxanaSultan <roxana.sultan@student.unitbv.ro>
2024-04-02 23:55:30 +0300

    Added a few definitions for some of the Board classes

--- CheckersImpl/CheckersImpl/Services/TileModel.cs
+++ CheckersImpl/CheckersImpl/Services/TileModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Media;
 
-namespace CheckersImpl.Services
+public class TileModel
 {
-    internal class TileModel
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public string Color { get; set; }
+    public bool IsOccupied { get; set; }
+
+    public TileModel(int row, int column, string color)
     {
+        Row = row;
+        Column = column;
+        Color = color;
+        IsOccupied = false;
+    }
+
+    public Color GetColor()
+    {
+        switch (Color.ToLower())
+        {
+            case "lightbrown":
+                return (Color)ColorConverter.ConvertFromString("#B79F6F");
+            case "darkbrown":
+                return (Color)ColorConverter.ConvertFromString("#826D42");
+            default:
+                return Colors.Transparent;
+        }
     }
 }
--- CheckersImpl/CheckersImpl/ViewModels/BoardVM.cs
+++ CheckersImpl/CheckersImpl/ViewModels/BoardVM.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using CheckersImpl.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace CheckersImpl.ViewModels
 {
     internal class BoardVM
     {
+        public ObservableCollection<TileModel> Board { get; set; }
+
+        public BoardVM()
+        {
+            Board = new ObservableCollection<TileModel>();
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    string color;
+                    if (row % 2 == 0)
+                    {
+                        color = (column % 2 == 0) ? "lightbrown" : "darkbrown";
+                    }
+                    else
+                    {
+                        color = (column % 2 == 0) ? "darkbrown" : "lightbrown";
+                    }
+                    Board.Add(new TileModel(row, column, color));
+                }
+            }
+        }
+
     }
 }
