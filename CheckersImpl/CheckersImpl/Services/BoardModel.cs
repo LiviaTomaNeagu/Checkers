eee8cf14c4632dc4e6a20badd980f9696a290be3
RoxanaSultan <roxana.sultan@student.unitbv.ro>
2024-04-03 13:02:45 +0300

    Added Board startup

--- CheckersImpl/CheckersImpl/Services/BoardModel.cs
+++ CheckersImpl/CheckersImpl/Services/BoardModel.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CheckersImpl.Services
 {
-    internal class BoardModel
+    public class BoardModel
     {
+        public int[,] BoardState { get; private set; }
+
+        public BoardModel()
+        {
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            // Initialize the board with the starting state
+            BoardState = new int[8, 8];
+            // Logic to set up the initial state of the board
+        }
+
+        // Other methods and properties related to the game board
     }
 }
+
