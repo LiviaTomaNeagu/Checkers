e2431696c043f2364c3271449f7732c67bbbc2f7
RoxanaSultan <roxana.sultan@student.unitbv.ro>
2024-04-03 14:49:21 +0300

    Created the board

--- CheckersImpl/CheckersImpl/Services/TileModel.cs
+++ CheckersImpl/CheckersImpl/Services/TileModel.cs
@@ -4,32 +4,14 @@ public class TileModel
 {
     public int Row { get; set; }
     public int Column { get; set; }
-    public Color Color { get; set; }
+    public SolidColorBrush Color { get; set; }
     public bool IsOccupied { get; set; }
 
-    public TileModel(int row, int column, string color)
+    public TileModel(int row, int column, SolidColorBrush color)
     {
         Row = row;
         Column = column;
-        Color = GetColorFromString(color);
+        Color = color;
         IsOccupied = false;
-    }
-
-    public Color GetColorFromString(string color)
-    {
-        switch (color.ToLower())
-        {
-            case "lightbrown":
-                return (Color)ColorConverter.ConvertFromString("#B79F6F");
-            case "darkbrown":
-                return (Color)ColorConverter.ConvertFromString("#826D42");
-            default:
-                return Colors.Transparent;
-        }
-    }
-
-    public Color GetColor()
-    {
-        return Color;
     }
 }
